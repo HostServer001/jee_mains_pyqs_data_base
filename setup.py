b0bb47b9d03936b2f8754e1352cafa771c1a1fc3
Jvk <tanishavkadam@gmail.com>
2025-11-14 02:08:42 +0530

    progress bar fix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="jee_data_base",
-    version="0.1.2.post8",
+    version="0.1.2.post9",
     packages=find_packages(),
     install_requires=[
         "hdbscan",
